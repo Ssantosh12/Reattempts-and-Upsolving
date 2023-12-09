28ec07208402508e79c54692aa76d2bd003a1598
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2023-12-09 13:52:22 +0530

    Update Chef and Numbers.cpp

--- Reattempts/Codechef/Chef and Numbers.cpp
+++ Reattempts/Codechef/Chef and Numbers.cpp
@@ -54,3 +54,49 @@ int main()
         }
     }
 }
+
+// OLD Solution using counting technique to contain a subarray instead of two pointers left and right
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int ele,n,i,continuous=0,maxChoice=0;
+        scanf("%d",&n);
+        map<int,int> choices;
+        int arr[n];
+        for(i=0;i<=n-1;i++)
+            scanf("%d",&arr[i]);
+        for(i=0;i<=n-1;i++){
+            continuous++;
+            if(i==(n-1)){
+                if(continuous%2==0)
+                    choices[arr[i]]=choices[arr[i]]+continuous/2;
+                else choices[arr[i]]=choices[arr[i]]+(continuous/2)+1;
+                continuous=0;
+            }
+            else if(arr[i]!=arr[i+1]){
+                if(continuous%2==0)
+                    choices[arr[i]]=choices[arr[i]]+continuous/2;
+                else choices[arr[i]]=choices[arr[i]]+(continuous/2)+1;
+                continuous=0;
+            }
+        }
+        for(auto x: choices){
+            if(x.second>maxChoice)
+                maxChoice=x.second;
+        }
+        for(auto x: choices){
+            if(x.second==maxChoice){
+                cout<<x.first<<"\n";
+                break;
+            }
+        }
+    }
+}
+*/
