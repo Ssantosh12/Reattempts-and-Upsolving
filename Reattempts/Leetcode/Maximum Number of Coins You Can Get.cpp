818d4cf01cbea27c709420036ed7a7a0b117b4fa
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2024-02-25 22:45:16 +0530

    Update Maximum Number of Coins You Can Get.cpp

--- Reattempts/Leetcode/Maximum Number of Coins You Can Get.cpp
+++ Reattempts/Leetcode/Maximum Number of Coins You Can Get.cpp
@@ -3,6 +3,7 @@ public:
     int maxCoins(vector<int>& piles) {
         // no matter what three piles we choose we will have to give the largest pile to Alice
         // so the best way is to choose first two as current 2 the maximums and last 1 minimum (ex:1 2 4 7 8 9 => 8 and 4 will be ours optimally)
+        
         int ind,times=piles.size()/3,coins=0;
         sort(piles.begin(),piles.end());
         ind=piles.size()-2;
