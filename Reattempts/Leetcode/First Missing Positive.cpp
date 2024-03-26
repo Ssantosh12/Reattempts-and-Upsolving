aa7277484fab22a1b2a822d77ca24f2cdebcebfb
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2024-03-26 10:06:50 +0530

    Update First Missing Positive.cpp

--- Reattempts/Leetcode/First Missing Positive.cpp
+++ Reattempts/Leetcode/First Missing Positive.cpp
@@ -16,3 +16,18 @@ public:
         return smallestPos;
     }
 };
+
+// OLDER SOLUTION : BETTER ONE
+/*
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int miss=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==miss) miss++;
+        }
+        return miss;
+    }
+};
+*/
